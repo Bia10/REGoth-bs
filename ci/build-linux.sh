780a0be284ea0dec0ee21da3a2cda0c32d4c4a1d
Andre Taulien <a.taulien@live.de>
2019-06-10 17:37:18 +0200

    Use system packages

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -9,13 +9,10 @@ wget https://dilborceisv8p.cloudfront.net/bsf_2019.06.06_linux.tar.gz
 mkdir bsf
 tar -xf bsf_2019.06.06_linux.tar.gz -C bsf
 
-vcpkg install libsquish physfs --triplet x64-linux
-
 cmake -Dbsf_INSTALL_DIR=`pwd`/bsf \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DREGOTH_USE_SYSTEM_BSF=On \
       -DSKIP_AUTOMATE_VCPKG=On \
-      -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" \
       ../..
 
 cmake --build . --config RelWithDebInfo --parallel
