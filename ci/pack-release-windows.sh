794b05982ad28f0f0c9306175361d4d940494ef6
Andre Taulien <a.taulien@live.de>
2019-06-14 14:55:38 +0200

    Fix artifacts

--- ci/pack-release-windows.sh
+++ ci/pack-release-windows.sh
@@ -9,6 +9,5 @@ mkdir $INTERNAL_DIR
 cp ../README.md $INTERNAL_DIR/
 cp ../LICENSE $INTERNAL_DIR/
 cp -r ../content $INTERNAL_DIR/
-cp -r build/bin/*.pdb $INTERNAL_DIR/
 cp -r build/bin/RelWithDebInfo/*.exe $INTERNAL_DIR/
 cp -r build/bin/RelWithDebInfo/*.dll $INTERNAL_DIR/
